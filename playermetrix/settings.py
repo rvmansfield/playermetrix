d33c56b0ca73cf0e75a59955aeb3300cb1f297e2
rvmansfield <rvmansfield@gmail.com>
2026-06-04 14:10:44 -0400

    Updated S3 Bucket info

--- playermetrix/settings.py
+++ playermetrix/settings.py
@@ -201,7 +201,9 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-    AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME", "us-east-1")
+    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
 
     AWS_QUERYSTRING_AUTH = False
     #AWS_DEFAULT_ACL = "public-read"
